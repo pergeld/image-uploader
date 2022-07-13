e7283b36e72d59d48c6d222cf12f7e56e2d814c9
pergeld <pergeldaniel@gmail.com>
2022-07-13 10:58:58 +0200

    Let's Add Vue

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -5,10 +5,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="max-w-lg mx-auto mt-24">
             <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+
+            <div id="app">
+
+            </div>
         </div>
     </body>
 </html>
