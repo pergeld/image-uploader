e34be81c9bfceec01107be25a7bc54723b06bc70
pergeld <pergeldaniel@gmail.com>
2022-07-13 11:31:37 +0200

    Saving Photo Data

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
@@ -18,6 +19,32 @@ class ImageController extends Controller
 
     public function store(Request $request)
     {
-        // save the image
+        // validate the incoming file
+        if (!$request->hasFile('image')) {
+            return response()->json(['error' => 'There is no image present.'], 400);
+        }
+
+        $request->validate([
+            'image' => 'required|file|image'
+        ]);
+
+        // save the file in storage
+        $path = $request->file('image')->store('public/images');
+
+        if (!$path) {
+            return response()->json(['error', 'The file could not be saved.'], 500);
+        }
+
+        $uploadedFile = $request->file('image');
+
+        // create image model
+        $image = Image::create([
+            'name' => $uploadedFile->hashName(),
+            'extension' => $uploadedFile->extension(),
+            'size' => $uploadedFile->getSize()
+        ]);
+
+        // return that image model back to the frontend
+        return $image;
     }
 }
--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -4,6 +4,7 @@
         <title>Image Uploading with Vue + Larave</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf_token" content="{{ csrf_token() }}">
         <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
